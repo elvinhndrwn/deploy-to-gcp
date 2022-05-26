c9e9e355d835716e808cb4006db9c927096e095c
elvinhndrwn <elvin.dwi@ist.id>
2022-05-27 02:38:55 +0700

    first commit

---
+++ src/main/java/com/example/deploytogcp/DeployToGcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploytogcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployToGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployToGcpApplication.class, args);
+	}
+
+}
